402cedad6843414853d3e97fa2aec18dd8137588
Callen :D <callen.lin1@education.nsw.gov.au>
2026-06-09 00:58:09 +0000

    feat: complete footer.

--- app.py
+++ app.py
@@ -12,6 +12,28 @@ def home():
 def contact():
     return render_template('contact.html')
 
+# MAIN PAGES
+
+
+@app.route('/terms-and-conditions')
+def terms_and_conditions():
+    return render_template('terms-and-conditions.html')
+
+
+@app.route('/support')
+def support():
+    return render_template('support.html')
+
+
+@app.route('/about-us')
+def about_us():
+    return render_template('about-us.html')
+
+
+@app.route('/site-map')
+def site_map():
+    return render_template('site-map.html')
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
